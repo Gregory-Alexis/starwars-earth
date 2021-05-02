d9e665dcd06850d5839c76905652c8cae87560e5
Westindiess <gregoryalexiss@gmail.com>
2021-05-02 16:36:46 +0200

    Sw

--- src/component/StarwarsApp.js
+++ src/component/StarwarsApp.js
@@ -5,6 +5,9 @@ const StarwarsApp = () => {
 	const [climats, setClimat] = useState("")
 	const [earthName, setEarthName] = useState([])
 	const [population, setPopulation] = useState("")
+	const [loading, setLoading] = useState(true)
+	const [readMore, setReadMore] = useState(false)
+
 	useEffect(() => {
 		const url = `https://swapi.dev/api/planets/`
 
@@ -17,35 +20,41 @@ const StarwarsApp = () => {
 			})
 			.then((data) => {
 				console.log(data)
-				setEarthName(data.results)
+				setEarthName((earthName) => [...earthName, ...data.results])
+				setLoading(false)
 			})
 			.catch((error) => {
 				alert(error.message)
 			})
 	}, [])
 	return (
-		<section className="container py-5 ">
-			<h1 className="mb-5 text-center">Planètes dans l'univers Star Wars</h1>
-			<div className="row">
-				<div className="col-md-6 col-lg-4 col-xl-3">
-					<article>
-						{earthName.map((el) => {
-							const { name, population, climate } = el
-							return (
-								<div key={name} className="bg-warning ps-2 border p-4">
+		<section>
+			<h1 className="container py-5 text-center">
+				Planètes dans l'univers Star Wars
+			</h1>
+			<div className="mb-5 ">
+				<div className="row justify-content-center">
+					{earthName.map((el) => {
+						const { name, population, climate } = el
+						return (
+							<div
+								key={name.name}
+								className="m-2 col-md-6 col-lg-4 col-xl-3 mb-4"
+							>
+								<article className="bg-warning p-3">
 									<h2>{name}</h2>
 									<p>
-										<b>POPULATION</b>
+										<b>population</b>
 									</p>
 									<p>{population}</p>
 									<p>
 										<b>Climat</b>
 									</p>
 									<p>{climate}</p>
-								</div>
-							)
-						})}
-					</article>
+								</article>
+							</div>
+						)
+					})}
 				</div>
 			</div>
 		</section>
